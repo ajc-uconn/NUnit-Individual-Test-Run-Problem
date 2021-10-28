e0b4c7d310019604ef8785726bf75be997163e2a
Anthony Castellano <anthony.castellano@uconn.edu>
2021-10-28 11:53:34 -0400

    Adding all sample code

---
+++ NUnitTestAdapterIndividualRunIssue/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace NUnitTestAdapterIndividualRunIssue
+{
+    [TestFixture]
+    internal class TestClass
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(true, true);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
